0fe71b9a27d67f05ab5cf73dba50338e7627afbd
A S Payal Thangamma <payalthangamma99@gmail.com>
2024-07-22 11:25:17 +0530

    Create CreationalSingleton.java

---
+++ CreationalSingleton.java
@@ -0,0 +1,21 @@
+//Use Case: Ensuring a single instance of a class.
+
+class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+        System.out.println(singleton1 == singleton2);  // Output: true
+    }
+}
