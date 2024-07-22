fa8b3c8696da57436dac881f30dd639e35c7340d
A S Payal Thangamma <payalthangamma99@gmail.com>
2024-07-22 11:26:22 +0530

    Create CreationalFactory.java

---
+++ CreationalFactory.java
@@ -0,0 +1,32 @@
+// Use case: Creating objects without specifying the exact class.
+public abstract class Animal {
+    public abstract String speak();
+}
+public class Dog extends Animal {
+    public String speak() {
+        return "Woof!";
+    }
+}
+public class Cat extends Animal {
+    public String speak() {
+        return "Meow!";
+    }
+}
+public class AnimalFactory {
+    public static Animal createAnimal(String type) {
+        switch (type.toLowerCase()) {
+            case "dog":
+                return new Dog();
+            case "cat":
+                return new Cat();
+            default:
+                return null;
+        }
+    }
+} 
+class Main {
+    public static void main(String[] args) {
+        Animal animal = AnimalFactory.createAnimal("dog");
+        System.out.println(animal.speak());
+    }
+}
