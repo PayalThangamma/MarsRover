59a32e643f918344fa2d9721d0bdd026c91ed4db
A S Payal Thangamma <payalthangamma99@gmail.com>
2024-07-22 11:41:14 +0530

    Update BehaviouralChangeOfResponsibility.java

--- BehaviouralChangeOfResponsibility.java
+++ BehaviouralChangeOfResponsibility.java
@@ -24,7 +24,7 @@ class ConcreteHandler2 extends Handler {
         }
     }
 }
-class Main {
+class BehaviouralChangeOfResponsibility {
     public static void main(String[] args) {
         Handler handler1 = new ConcreteHandler1();
         Handler handler2 = new ConcreteHandler2();
